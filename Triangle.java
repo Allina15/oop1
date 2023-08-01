5e23a0887632222ae41e8496382901bf1cf7786c
Allina15 <140904065+Allina15@users.noreply.github.com>
2023-08-01 15:25:00 +0600

    Add files via upload

---
+++ Triangle.java
@@ -0,0 +1,12 @@
+import static java.lang.Math.sqrt;
+
+public class Triangle {
+    double a;
+    double b;
+    double c;
+
+    public double area(){
+       double area = (a*b)/2;
+       return area;
+    }
+}
